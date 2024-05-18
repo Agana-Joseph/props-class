bcb6ae3e9400eb1585aaf3266ba7cc8e26b73281
Agana-Joseph <josephagana8@gmail.com>
2024-05-18 17:36:36 +0100

    React countdown

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+
 const App = ()=>{
-  let [details, setDetails] = useState([
-    {
-      name: "Taye",
-      goods: "clothes",
-      qty: 10
-    },
-    {
-      name: "Kenny",
-      goods: "Bags",
-      qty: 20
-    },
-    {
-      name: "Idowu",
-      goods: "shoes",
-      qty: 30
-    }
-  ]);
-  const increment = (i)=>{
-    let allDetails = [...details];
-    let foundDetail = allDetails.find((_, index)=>index===i);
-    foundDetail.qty++;
-    setDetails(allDetails);
-  }
+
+ 
   return(
     <div>
-      <Header/>
-     {details.map((detail, i)=>(
-      <Main key={i} index={i} name={detail.name} goods={detail.goods} qty={detail.qty} increase={increment}/>
-     ))}
-     
-     <Footer/>
       
     </div>
   );
