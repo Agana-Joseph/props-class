c0bfd8fa5f58748c8169199561a3db387b7d9ff4
Agana-Joseph <josephagana8@gmail.com>
2024-05-19 07:54:40 +0100

    Counter

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import axios from 'axios'
 
 
 const App = ()=>{
-
+  let [inputVal, setInputVal] = useState ("");
+  let [myVal, setMyVal] = useState(0);
+  useEffect(()=>{
+    let myInterval;
+    if (myVal > 0){
+     myInterval = setInterval(() => {
+      setMyVal(myVal - 1);
+    }, 1000);
+  }
+    return ()=>{
+      clearInterval(myInterval);
+    }
+  }, [myVal]);
  
   return(
     <div>
-      
+      <input onInput={e=>setInputVal(e.target.value)}/>
+      <button onClick={() =>setMyVal(inputVal)}>click here</button>
+      {myVal}
     </div>
   );
 
